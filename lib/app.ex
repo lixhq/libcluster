c122840a8fb597558b90dbb56038f85c47b20942
Emil Ingerslev <emil.ingerslev@gmail.com>
2017-03-09 14:44:26 +0100

    Add disable topology option for runtime flexibility
    By setting a topology's `:enabled` to `false` or using it's
    {:config, <environment variable name>} option, it is possible
    to disable specific topologies runtime, thus making a built
    docker image usable in multiple use cases, fx local test and
    k8s production

--- lib/app.ex
+++ lib/app.ex
@@ -14,20 +14,26 @@ defmodule Cluster.App do
     import Supervisor.Spec, warn: false
     specs = Application.get_env(:libcluster, :topologies, [])
     for {topology, spec} <- specs do
-      strategy       = Keyword.fetch!(spec, :strategy)
-      config         = Keyword.get(spec, :config, [])
-      connect_mfa    = Keyword.get(spec, :connect, {:net_kernel, :connect, []})
-      disconnect_mfa = Keyword.get(spec, :disconnect, {:net_kernel, :disconnect, []})
-      list_nodes_mfa = Keyword.get(spec, :list_nodes, {:erlang, :nodes, [:connected]})
-      opts           = Keyword.get(spec, :child_spec, [])
-      worker_args = [[
-        topology: topology,
-        connect: connect_mfa,
-        disconnect: disconnect_mfa,
-        list_nodes: list_nodes_mfa,
-        config: config
-      ]]
-      worker(strategy, worker_args, opts)
+      enabled = case Keyword.get(spec, :enabled, true) do
+        {:system, env_key} -> System.get_env(env_key) === "true"
+        value -> value
+      end
+      if enabled do
+        strategy       = Keyword.fetch!(spec, :strategy)
+        config         = Keyword.get(spec, :config, [])
+        connect_mfa    = Keyword.get(spec, :connect, {:net_kernel, :connect, []})
+        disconnect_mfa = Keyword.get(spec, :disconnect, {:net_kernel, :disconnect, []})
+        list_nodes_mfa = Keyword.get(spec, :list_nodes, {:erlang, :nodes, [:connected]})
+        opts           = Keyword.get(spec, :child_spec, [])
+        worker_args = [[
+          topology: topology,
+          connect: connect_mfa,
+          disconnect: disconnect_mfa,
+          list_nodes: list_nodes_mfa,
+          config: config
+        ]]
+        worker(strategy, worker_args, opts)
+      end
     end
   end
 end
